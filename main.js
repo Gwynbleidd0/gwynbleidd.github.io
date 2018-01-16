86fbc0a047dbf7582979561ff987d594b8a7c5c8
Gwynbleidd0 <35406689+Gwynbleidd0@users.noreply.github.com>
2018-01-16 16:32:24 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,71 @@ $(document).ready(function(){
         
 
     });
+    var sv1 = true;
+    var sv2 = true;
+    var sv3 = true;
 
+        $("#b1").click(function(){
+            if (sv1 == true) {
+                $("#b1").animate({height:'+=10',top:'+=300'}, "slow");
+            $("#c1").animate({height:'+=310'}, "slow");
+            
+            sv1 = false
+            }
+            else {
+                
+                $("#c1").animate({height:'-=310'}, "slow");
+                $("#b1").animate({height:'-=10',top:'-=300'}, "slow");
+                
+                sv1 = true
+            }
+            
+    });
+    $("#b2").click(function(){
+            if (sv2 == true) {
+            $("#b2").animate({height:'+=10',top:'+=300'}, "slow");
+            $("#c2").animate({height:'+=310'}, "slow");
+        
+            sv2 = false
+            }
+            else {
+            
+            $("#c2").animate({height:'-=310'}, "slow");
+            $("#b2").animate({height:'-=10',top:'-=300'}, "slow");
+            
+            sv2 = true
+            }
+        
+        });
+        $("#b3").click(function(){
+            if (sv3 == true) {
+            $("#b3").animate({height:'+=10',top:'+=300'}, "slow");
+            $("#c3").animate({height:'+=310'}, "slow");
+        
+            sv3 = false
+            }
+            else {
+            
+            $("#c3").animate({height:'-=310'}, "slow");
+            $("#b3").animate({height:'-=10',top:'-=300'}, "slow");
+            
+            sv3 = true
+            }
+        
+        });
+    for(var i=0;i < 10, i++;){
+    $("#i1").attr("src","images/im1.jpg");
+    setTimeout(5000);
+    $("#i1").attr("src","images/im2.jpg");
+    setTimeout(5000);
+    $("#i1").attr("src","images/im3.jpg");
+    setTimeout(5000);
+    }
 });
-$(".scroll").click(function(){
-    $(".sv").animate({height:"+=400"}, slow)
-});
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+      center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
